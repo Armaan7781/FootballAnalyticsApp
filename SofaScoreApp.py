7de8b689946f6656e2be5d23fb52057ba2a40eb0
Armaan7781 <armaanmulla7781@gmail.com>
2026-06-13 12:24:39 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- SofaScoreApp.py
+++ SofaScoreApp.py
@@ -236,6 +236,10 @@ def load_and_preprocess_data():
     try:
         url = "https://raw.githubusercontent.com/Armaan7781/FootballAnalyticsApp/main/Historical%20Data.csv"
         df = pd.read_csv(url).fillna(0)
+        df['player'] = df['player'].apply(
+    lambda name: ''.join(
+        c for c in unicodedata.normalize('NFD', str(name))
+        if unicodedata.category(c) != 'Mn'))
         df['is_gk'] = (df['saves'] > 0) | (df['savesParried'] > 0) | (df['punches'] > 0) | (df['highClaims'] > 0)
         return df
     except Exception as e:
